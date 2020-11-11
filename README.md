48b70b7cbfeecc8e0aefd62e73ef0eecb65b9aa4
unknown <sallyannegaller@gmail.com>
2020-11-11 15:57:33 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bookmarks Server
+This is an API for the Bookmarks client project, supporting GET, POST, and DELETE requests. 
---
+++ src/store.js
@@ -0,0 +1,18 @@
+const bookmarks = [
+  {
+    id: 1,
+    title: "Bookmark 1",
+    url: "www.bookmark.com",
+    description: "Test bookmark",
+    rating: 4,
+  },
+  {
+    id: 2,
+    title: "Bookmark 2",
+    url: "www.bookmark2.com",
+    description: "Another test bookmark",
+    rating: 3,
+  },
+];
+
+module.exports = { bookmarks };
